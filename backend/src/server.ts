a65da1dbb7f7eeefab93021d32b50c6989ecb545
Gabriel Costa <gabrielestevan14@gmail.com>
2020-10-19 16:03:02 -0300

    Adding pastes of project

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import "./database/connection";
+import "express-async-errors";
+import errorHandler from './errors/handle';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')));
+app.use(errorHandler);
+
+app.listen(3333);
